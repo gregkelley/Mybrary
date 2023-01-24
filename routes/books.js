60e00aec92df3de0152e5391ba700dc07fed2e7b
Greg Kelley <1431374+gregkelley@users.noreply.github.com>
2023-01-24 11:21:25 -0700

    Finished Kyles code for managaging Books. Course #6

--- routes/books.js
+++ routes/books.js
@@ -70,8 +70,8 @@ router.post('/', async (req, res) => {
 
   try {
     const newBook = await book.save();
-    // res.redirect(`books/${newBook.id}`)
-    res.redirect(`books`);
+    res.redirect(`books/${newBook.id}`)
+    // res.redirect(`books`);
   } catch {
     renderNewPage(res, book, true);
   }
@@ -80,22 +80,7 @@ router.post('/', async (req, res) => {
 // move new book page render out of the route because it needs to be accessed from
 // more than one place.
 async function renderNewPage(res, book, hasError = false) {
-  try {
-    const authors = await Author.find({});
-    // deal with an error situation
-    const params = {
-      authors: authors,
-      book: book,
-    };
-    if (hasError) {
-      params.errorMessage = 'Error Creating Book';
-    }
-
-    // const book = new Book();
-    res.render('books/new', params);
-  } catch {
-    res.redirect('/books');
-  }
+  renderFormPage(res, book, 'new', hasError);
 }
 
 function saveCover(book, coverEncoded) {
@@ -109,25 +94,94 @@ function saveCover(book, coverEncoded) {
   }
 }
 
+// Show book route
 // show book info. cover, author, date, count, desc. then edit, delete buttons and 
-// author link button. Receives book.id
+// author link button. Receives book.id.  Course #6
 router.get('/:id', async (req, res) =>{
   try {
-    // const author = await Author.findById(req.params.id);
-    const book = await Book.findById(req.params.id);
-    // if (book.length < 1) console.log('no book found');
-    const authorName = await Author.findById(book.author);
+    // console.log('show book')
+    // my code. works.
+    // const book = await Book.findById(req.params.id);
+    // const authorName = await Author.findById(book.author);
+
+    // use populate to get the author info for the book.
+    const book = await Book.findById(req.params.id).populate('author').exec();
     res.render('books/show', 
-        { book: book, 
-          authorName: authorName,
-        });
-  } catch (e) {
-    console.log(e);
+        { book: book });
+  } catch {
     res.redirect('/');
   }
 });
 
-// delete some book. I copied and mod'd from author
+// edit das book
+router.get('/:id/edit', async (req, res) => {
+  // console.log('edit book', req.params.id);
+  try {
+    const book = await Book.findById(req.params.id);
+    renderEditPage(res, book);
+  } catch {
+    res.redirect('/');
+  }
+});
+
+async function renderEditPage(res, book, hasError = false) {
+  // console.log('render edit book', book.id);
+  renderFormPage(res, book, 'edit', hasError);
+}
+
+async function renderFormPage(res, book, form, hasError = false) {
+  try {
+    const authors = await Author.find({});
+    // deal with an error situation
+    const params = {
+      authors: authors,
+      book: book,
+    };
+    if (hasError) {
+      // because of code reuse, need another if stmt here to make the error
+      // msg more useful. @15:00 of course #6
+      params.errorMessage = `Error ${form} Book`;
+    }
+
+    // const book = new Book();
+    res.render(`books/${form}`, params);
+  } catch {
+    res.redirect('/books');
+  }
+}
+
+// Update a book route
+router.put('/:id', async (req, res) => {
+
+  // construct book object
+  let book 
+
+  try {
+    book = await Book.findById(req.params.id);
+    book.title = req.body.title
+    book.author = req.body.author
+    book.publishDate = new Date(req.body.publishDate)
+    book.pageCount = req.body.pageCount
+    book.description = req.body.description
+    if (req.body.cover != null && req.body.cover !== '') {
+      // if a new cover was provided, save it
+      saveCover(book, req.body.cover);
+    }
+    // save the new book info then go to the show book page
+    await book.save();
+    res.redirect(`/books/${book.id}`);
+  } catch {
+    if ( book != null ) {
+      // we were able to get the book but we failed to save it. Redirect to edit page
+      renderEditPage(res, book, true);
+    } else {
+      redirect('/');
+    }
+    renderNewPage(res, book, true);
+  }
+});
+
+// delete some book. I copied and mod'd from author code
 router.delete('/:id', async (req, res) =>{
   let book;
 
@@ -142,8 +196,10 @@ router.delete('/:id', async (req, res) =>{
     if (book == null) {
       res.redirect('/')
     } else {
-      // res.redirect(`/books/${book.id}`);
-      res.redirect('/books');
+      res.redirect('/books/show', {
+        book: book,
+        errorMessage: 'Could not remove book'
+      });
     }
   }
 });
